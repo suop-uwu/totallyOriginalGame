8c7dc482dd66447a97ba080d36dfcd5d7ae778bb
uL-hpesoJ <Josephlu8@gmail.com>
2017-12-04 08:12:47 -0700

    uh probably 0.03
    added a bunch of stuff related to movement

--- scripts/platformer.js
+++ scripts/platformer.js
@@ -49,13 +49,18 @@ $(function () {
     }
 
     //compares to see if num1 and 2 are within range of each other. returns true or false
-    function isWithin(num1, num2, range) {
-        if (Math.sqrt(Math.pow(num1 - num2, 2)) < range) {
+    function isWithin(num, range1, range2) {
+        if (num >= range1 && num <= range2) {
             return true;
         } else {
             return false;
         }
     }
+
+    function delay(code, timeMs) {
+        window.setInterval(code(), timeMs);
+    }
+
     var blocks = {
         basic: loadImage('img/sprites/blocks/basic.png')
     };
@@ -64,7 +69,7 @@ $(function () {
         jumpsquat: [loadImage('img/sprites/zeeTee/jumpsquatL.png'), loadImage('img/sprites/zeeTee/jumpsquatR.png')],
         currentSprite: loadImage('img/sprites/zeeTee/idleR.png'),
         facing: 1, // 0 is left, 1 is right
-        x: 17.5,
+        x: 17,
         y: 10,
         velx: 0,
         vely: 1,
@@ -72,11 +77,11 @@ $(function () {
         accelx: 0.05,
         airAccelx: 0.04,
         friction: 0.1,
-        airFriction: 0.01,
+        airFriction: 0.05,
         walkSpeed: 0.15,
         runSpeed: 0.3,
         gravity: 0.1,
-        fallSpeed: 1.75,
+        fallSpeed: 4,
         fullHop: 1.6, //enough to jump around 5 blocks
         shortHop: 0.8,
         jumpsquatDuration: 4, //in frames
@@ -97,6 +102,7 @@ $(function () {
         return temp;
     }
 
+    //keyhandler
     $(document).keydown(function (event, char) {
         char = event.which; //identify what char was pressed
         keysDown[event.keyCode] = true;
@@ -227,82 +233,58 @@ $(function () {
         }
     }
 
-    function updatePos() {
-        var tempPartOfArray = 1;
-        var tempOffset = 0;
-        var tempOffset2 = -1;
-        var currentModdedVel = mc.velx * mc.xModifier;
-
-        //x
-        if (currentModdedVel > 0) { //is going right
-            tempPartOfArray = 0;
-            tempOffset = 1;
-            tempOffset2 = 2;
-        } else if (typeof currentModdedVel !== 'number') { //error message
-            console.log('horizontal velocity is invalid somehow. currently returns \'' + currentModdedVel + '\'');
-        }
-        if (collisions[Math.trunc(mc.x) + tempOffset] && Array.isArray(collisions[Math.trunc(mc.x + tempOffset)][Math.trunc(mc.y)]) !== true && //if block 2 to right / left is not an array
-            collisions[Math.trunc(mc.x) + tempOffset2] && Array.isArray(collisions[Math.trunc(mc.x + tempOffset2)][Math.trunc(mc.y)]) !== true) { //if block to right / left is not an array
-            mc.x += currentModdedVel;
-        } else { //if it is
-            switch (tempOffset === 0) {
-                case true: //left
-                    if (collisions[Math.trunc(mc.x) - 1] && collisions[Math.trunc(mc.x) - 1][Math.trunc(mc.y)] && //if it exists
-                        mc.x + currentModdedVel <= collisions[Math.trunc(mc.x) - 1][Math.trunc(mc.y)][1]) { // if pos of next frame is less than rightmost collision of block to left
-                        mc.x = Math.trunc(mc.x);
-                        currentModdedVel = 0;
-                    } else {
-                        mc.x += currentModdedVel;
-
-                    }
-                    break;
-                case false: //right
-                    if (collisions[Math.trunc(mc.x + 1.5)] && collisions[Math.trunc(mc.x + 1.5)][Math.trunc(mc.y)] && //if it exists
-                        mc.x + currentModdedVel + mc.width >= collisions[Math.trunc(mc.x + 1.5)][Math.trunc(mc.y)][0]) { // if pos of next frame is greater than
-                        mc.x = Math.trunc(mc.x + 0.5);
-                        currentModdedVel = 0;
-                    } else {
-                        mc.x += currentModdedVel;
-                    }
-                    break;
-            }
-        }
-
-
-
-        //y
-        for (let i = 0; i < 2; i++) {
-            $.each(collisions[Math.trunc(mc.x) + i], function (index, val) {
-
-                if (mc.y + mc.vely / mc.jumpSpeed < val[3] && mc.onGround === false && mc.y > val[3] && mc.x + 1 !== val[0]) { //if y on next frame is less than top collision and y is  greater than top value.
-                    mc.onGround = true;
-                    mc.y = val[3];
-                    mc.vely = 0;
-                    mc.state = 'landing';
-                    window.setTimeout(function () {
-                        mc.state = 'idle'; //TODO make these work
-                    }, (1000 / 60) * 6);
-                }
-
-                if (mc.y + mc.height + mc.vely / mc.jumpSpeed > val[2] && mc.y + mc.height <= val[2] && mc.x + 1 !== val[0]) { // if top of char on next frame will be greater than bottom collision.
-                    if (mc.y + 1 === val[2]) {
-                        mc.vely = 0;
-                        mc.onGround = true;
-                    } else {
-                        mc.y = val[2] - mc.height;
-                        mc.vely = 0;
-                    }
-                }
-
-            });
-        }
-        if (mc.onGround === false) { //changes y by vel / 3. works because if mc hits a block, vel is set to 0
-            mc.y += mc.vely / 3;
-        }
+    function updatePos() { //TODO clean up
+        var currentModdedVelx = mc.velx * mc.xModifier;
+        var currentXdir = -1;
+        var currentYdir = -1;
+        if (currentModdedVelx > 0)
+            currentXdir = 1;
+        if (mc.vely > 0)
+            currentYdir = 1;
+        mc.x += currentModdedVelx;
+        mc.y += mc.vely / mc.jumpSpeed;
 
     }
 
+    function doColisionThingies() {
+        //vertical collisions
+        for (let i = 0; i < 2; i++) { //repeat twice
+            for (let i2 = 0; i2 < 2; i2++) {
+                if (collisions[Math.trunc(mc.x + i * mc.width)] !== undefined && collisions[Math.trunc(mc.x + i * mc.width)][Math.trunc(mc.y + i2 * mc.height)] !== undefined) { //if it exists
+                    var collision = collisions[Math.trunc(mc.x + i * mc.width)][Math.trunc(mc.y + i2 * mc.height)]; //for ease of access
+                    if (mc.y + i2 * mc.height <= collision[3] && mc.y + i2 * mc.height >= collision[2]) { //if bottom of char is within block in terms of y
+                        mc.vely = 0;
+                        switch (i2) {
+                            case 1: //if it is detecting top hitbox
+                                mc.y = collision[2] - 1;
+                                break;
+                            case 0: //bottom hitbox
+                                mc.y = collision[3];
+                                mc.onGround = true;
+                                mc.state = "idle";
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        //horizontal collisions
+        for (let i = 0; i < 2; i++) {
+            for (let i2 = 0; i < 2; i++) {
+                if (collisions[Math.trunc(mc.x + i * mc.width)] !== undefined && collisions[Math.trunc(mc.x + i * mc.width)][mc.y + i2 * mc.width] !== undefined) {
+                    var collision = collisions[Math.trunc(mc.x + i * mc.width)][mc.y + i2 * mc.width];
+                    console.log('arewithin');
+                    if (isWithin(mc.x + i * mc.width, collision[0], collision[2])) {
+                        mc.x = Math.round(mc.x);
+                    }
+                }
+            }
+        }
+    }
+
+    //TODO implement a block detection system using the previous coordinates so it knows what side of the block you are on. for efficiecny it may work to put the update pos function inside of the collisions function since you need two values for the position. as for ground collision, just use math to find the two blocks that you are on, and if you are on air in one of them or both of them.
     function updateGroundState() { //supposed to detect when player walks off platform
+        console.log('ground state  updated');
         if (collisions[Math.round(mc.x)] !== undefined && collisions[Math.round(mc.x - 1)] !== undefined && collisions[Math.round(mc.x + 1)] !== undefined && //if it exists
             Array.isArray(collisions[Math.round(mc.x)][Math.trunc(mc.y - 1)]) === false) { //or not an array
             if (Math.round(mc.x) === Math.trunc(mc.x)) {
@@ -346,6 +328,16 @@ $(function () {
             ctx.fillText('onground: ' + mc.onGround, 0, 160);
             ctx.fillText('facing: ' + mc.facing, 0, 180);
             ctx.fillText('state: ' + mc.state, 0, 200);
+            //debug grid
+            for (let i = 0; i < 30; i++) {
+                for (let j = 0; j < Math.round(canvas.height / blockSize) + 1; j++) {
+                    ctx.strokeStyle = "#000";
+                    ctx.beginPath();
+                    ctx.rect(i * blockSize, canvas.height - j * blockSize, blockSize, blockSize);
+                    ctx.stroke();
+                }
+
+            }
         }
     }
 
@@ -408,12 +400,13 @@ $(function () {
         switch (mode) {
             case 'menu':
                 ctx.clearRect(0, 0, canvas.width, canvas.height + blockSize);
-                if (mc.onGround === true) {
-                    updateGroundState();
-                }
                 updateHorVel();
                 updateVerVel();
                 updatePos();
+                doColisionThingies();
+                if (mc.onGround === true) {
+                    updateGroundState();
+                }
                 updateFacing();
                 drawStage();
                 updateSprite();
